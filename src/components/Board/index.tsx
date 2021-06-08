2391515ae36dec0b0ac001bab1cb9134f74b9a5e
João Bispo <joaobispo2077@gmail.com>
2021-06-08 08:32:48 -0300

    feat: verify if drop card is the same that drag card

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,9 +1,24 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
+import {
+	DragDropContext,
+	DraggableLocation,
+	DropResult,
+} from 'react-beautiful-dnd';
 
 import { ICardList } from '../../interfaces/Card';
 import CardList from '../CardList';
 import { Container } from './styles';
+
+const isDropInActualPosition = (
+	source: DraggableLocation,
+	target: DraggableLocation,
+): boolean => {
+	const isSameIndex = source.index === target.index;
+	const isSameColumn = source.droppableId === target.droppableId;
+
+	const isSameCard = isSameIndex && isSameColumn;
+	return isSameCard;
+};
 
 export default function Board() {
 	const [cardlists, setCardLists] = useState<ICardList[]>();
@@ -18,10 +33,19 @@ export default function Board() {
 		return cardsLists;
 	};
 
-	const onDragEnd = useCallback((result) => {
+	const onDragEnd = useCallback((result: DropResult) => {
 		const { source, destination, draggableId } = result;
 
-		console.log(source, destination, draggableId);
+		if (!destination) return;
+
+		if (isDropInActualPosition(source, destination)) return;
+
+		const draggedIndex = source.index;
+		const droppedIndex = destination.index;
+
+		console.log(cardlists, draggedIndex, droppedIndex);
+
+		console.log(Date.now(), source, destination, draggableId);
 	}, []);
 
 	return (
